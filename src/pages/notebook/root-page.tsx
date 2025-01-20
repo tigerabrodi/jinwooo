04dc013dd8967217d774ab7240137cfad525a196
Tiger Abrodi <tigerabrodi@gmail.com>
2025-01-20 19:16:58 +0100

    feat: sign out button

--- src/pages/notebook/root-page.tsx
+++ src/pages/notebook/root-page.tsx
@@ -5,15 +5,15 @@ import { api } from '@convex/_generated/api'
 import { useConvexAuth, useQuery } from 'convex/react'
 import { Loader2 } from 'lucide-react'
 import { useEffect } from 'react'
-import { Outlet, useNavigate } from 'react-router'
+import { Outlet, useNavigate, useParams } from 'react-router'
 import { FolderTree } from './components/root/folder-tree'
-import { Sidebar } from './components/root/sidebar'
 
 export function RootPage() {
   const user = useQuery(api.users.getCurrentUser)
   const state = useConvexAuth()
   const isLoading = user === undefined || state.isLoading
   const navigate = useNavigate()
+  const { folderId } = useParams<{ folderId: string }>()
 
   const { signOut } = useAuthActions()
 
@@ -22,6 +22,14 @@ export function RootPage() {
       void navigate(ROUTES.authEntry)
     }
   }, [isLoading, user, navigate])
+
+  useEffect(() => {
+    // Is the user trying to navigate to `/notebook`?
+    // redirect to their initial folder
+    if (!isLoading && user && !folderId) {
+      void navigate(ROUTES.authEntry)
+    }
+  }, [folderId, isLoading, navigate, user])
 
   if (isLoading) {
     return (
@@ -33,18 +41,17 @@ export function RootPage() {
 
   return (
     <div className="container mx-auto flex h-full flex-1 rounded-lg border bg-background text-foreground">
-      {/* Sidebar */}
-      <Sidebar>
+      <div className="flex w-64 flex-col border-r border-border bg-sidebar">
         <FolderTree />
-        <Button className="" onClick={() => void signOut()}>
-          Logout
-        </Button>
-      </Sidebar>
+
+        <div className="p-2">
+          <Button className="" onClick={() => void signOut()} variant="ghost">
+            Logout
+          </Button>
+        </div>
+      </div>
 
       <Outlet />
-
-      {/* <NoteListPanel />
-      <NoteEditorPanel /> */}
     </div>
   )
 }
